4f0318712091376b6c14141e39a573cb1b19e12e
KIHWANBAE <62130418+footix01@users.noreply.github.com>
2021-01-16 17:28:14 +0900

    Initial commit

---
+++ Labs/Lab0/hypotenuse.py
@@ -0,0 +1,19 @@
+def CalculateHypotenuse(a, b):
+    import math
+    hypotenuse = math.sqrt(a**2+b**2)
+    return hypotenuse
+
+if __name__ == "__main__":
+    print(CalculateHypotenuse(10, 25))
+
+def CalculateAdd(a,b):
+    return a+b
+
+def CalculateSubtract(a,b):
+    return a-b
+
+def CalculateMultiply(a,b):
+    return a*b
+
+def CalculateDivide(a,b):
+    return a/b
